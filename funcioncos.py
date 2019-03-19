359d9168e213a96047605c4626c0c5d0a15ab5ef
Nicolás Nuñez <nnunez@df.uba.ar>
2019-03-19 16:42:20 -0300

    funcion prueba

---
+++ funcioncos.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 16:33:34 2019
+
+@author: nicon
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,5*np.pi,50)
+
+def function(A,w,x):
+    f = A*np.cos(w*x)
+    return f
+
+plt.plot(x,function(10,1,x))
+plt.show()
+    
+
+
