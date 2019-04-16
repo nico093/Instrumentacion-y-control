16bf0e759495ba6d1e1900f00e4f266367a65abf
Nicolás Nuñez <nnunez@df.uba.ar>
2019-04-16 17:28:19 -0300

    le pusimos para que labure con lantz

--- GeneradorFunciones.py
+++ GeneradorFunciones.py
@@ -10,54 +10,45 @@ Created on Tue Apr  9 15:30:09 2019
 Created on Sat Mar 30 19:16:28 2019
 @author: nicon
 """
-import pyvisa
 import numpy as np
 import time
+import lantz 
+from lantz import MessageBasedDriver
+from lantz.core import mfeats
+from Osciloscopio import Osciloscopio
 
-class GeneradorFunciones(object):
+class GeneradorFunciones(MessageBasedDriver):
 
-    def __init__(self, instrument = 0):
-        self.rm = pyvisa.ResourceManager()
-        if len(self.rm.list_resources()) > 0:
-            self.inst = self.rm.open_resource(self.rm.list_resources()[instrument])
-        else:
-            self.inst = []
-            print('No se detectó ningún instrumento')
-        if self.inst != []:
-            try:
-                print('El IDN del instrumento es ', self.inst.query("*IDN?"))
-            except:
-                print('El instrumento no respondió cuando se le preguntó el nombre.')
-    
+        
     def TurnOn(self, channel = 1):
-        self.inst.write("OUTPut{}:STATe ON".format(channel))
+        self.write("OUTPut{}:STATe ON".format(channel))
         
     def TurnOff(self, channel = 1):
-        self.inst.write("OUTPut{}:STATe OFF".format(channel))
+        self.write("OUTPut{}:STATe OFF".format(channel))
         
     def GetFrequency(self, channel = 1):
-        return self.inst.query('SOURce{}:FREQuency?'.format(channel))
+        return self.query('SOURce{}:FREQuency?'.format(channel))
         
     def SetFrequency(self, freq, channel = 1): #gen.SetFrequency('5 kHz') o por default en Hz
-        self.inst.write("SOURce{}:FREQuency {}".format(channel,freq))
+        self.write("SOURce{}:FREQuency {}".format(channel,freq))
     
     def GetShape(self, channel = 1):
-        return self.inst.query('SOURce{}:FUNCtion:SHAPe?'.format(channel))
+        return self.query('SOURce{}:FUNCtion:SHAPe?'.format(channel))
     
     def SetShape(self, shape, channel = 1): #gen.SetShape('SQUare')
-        self.inst.write("SOURce{}:FUNCtion {}".format(channel,shape)) 
+        self.write("SOURce{}:FUNCtion {}".format(channel,shape)) 
     
     def GetVoltage(self, channel = 1):
-        return self.inst.query('SOURce{}:VOLTage:LEVel:IMMediate:AMPLitude?'.format(channel))
+        return self.query('SOURce{}:VOLTage:LEVel:IMMediate:AMPLitude?'.format(channel))
     
     def SetVoltage(self, voltage, channel = 1): #gen.SetVoltage(2) Vpp
-        self.inst.write('SOURce{}:VOLTage:LEVel:IMMediate:AMPLitude {}'.format(channel, voltage))
+        self.write('SOURce{}:VOLTage:LEVel:IMMediate:AMPLitude {}'.format(channel, voltage))
         
     def GetOffset(self, channel = 1):
-        return self.inst.query('SOURce{}:VOLTage:LEVel:IMMediate:OFFSet?'.format(channel))        
+        return self.query('SOURce{}:VOLTage:LEVel:IMMediate:OFFSet?'.format(channel))        
     
     def SetOffset(self, offset, channel = 1): #gen.SetOffset(1) V
-        self.inst.write('SOURce{}:VOLTage:LEVel:IMMediate:OFFSet {}'.format(channel, offset))        
+        self.write('SOURce{}:VOLTage:LEVel:IMMediate:OFFSet {}'.format(channel, offset))        
     
     def GeneralSet(self, freq, voltage, offset = '0 V', shape = 'SIN', channel = 1):
         self.SetFrequency(freq, channel)
@@ -74,7 +65,20 @@ class GeneradorFunciones(object):
             time.sleep(timeoff)
         
     def ContinuosSweep(self, freqini, freqfin, sweeptime, sweeptype = "LINear", channel = 1):
-        self.inst.write('SOURce{}:SWEep:SPACing {}'.format(channel, sweeptype))
-        self.inst.write('SOURce{}:SWEep:TIME {}'.format(channel, sweeptime))    
-        self.inst.write('SOURce{}:FREQuency:STARt {}'.format(channel, freqini))
-        self.inst.write('SOURce{}:FREQuency:STOP {}'.format(channel, freqfin))
+        self.write('SOURce{}:SWEep:SPACing {}'.format(channel, sweeptype))
+        self.write('SOURce{}:SWEep:TIME {}'.format(channel, sweeptime))    
+        self.write('SOURce{}:FREQuency:STARt {}'.format(channel, freqini))
+        self.write('SOURce{}:FREQuency:STOP {}'.format(channel, freqfin))
+
+
+#%%
+        
+with GeneradorFunciones.via_usb('C033248') as Genf:
+    with Osciloscopio.Osciloscopio.via_usb('C102223') as Osc:
+        print(Genf.GetFrequency())
+        
+
+
+
+
+
