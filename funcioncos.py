1e69e2efff808e74bff18b7128808ced4b7403b9
Nicolás Nuñez <nnunez@df.uba.ar>
2019-04-16 17:29:22 -0300

    quse you

--- funcioncos.py
+++ funcioncos.py
@@ -7,15 +7,23 @@ Created on Tue Mar 19 16:33:34 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0,5*np.pi,50)
+#x = np.linspace(0,5*np.pi,50)
 
-def function(A,w,x):
-    f = A*np.cos(w*x)
-    return f
+#def function(A,w,x):
+#    f = A*np.cos(w*x)
+#    return f
 
-plt.plot(x,function(10,1,x))
+#plt.plot(x,function(10,1,x))
 #plt.plot(x,function(5,1,x),'r')
-plt.show()
+#plt.show()
     
 
+def GraficarCoseno(A, w):
+    periodo = 2*np.pi*(w**(-1))
+    print(periodo)
+    x = np.linspace(0, 2*periodo, 100)
+    cosx = [A*np.cos(w*i) for i in x]
+    plt.plot(x, cosx)
+    plt.show()
 
+GraficarCoseno(1, 2)
